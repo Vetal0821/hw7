4af54e0d7b6e1dcdd125f6248de779a41d1c8cd4
Vetal0821 <vetal0821@gmail.com>
2020-12-21 18:19:07 +0200

    hw7
    hw7WithoutHeroku

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :title, length: { maximum: 20 }
+  validates :content, length: { maximum: 300 }
+end
